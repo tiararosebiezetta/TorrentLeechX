d4f961dfb7f66e102325148304e6a84dc6728da2
Tiararose Biezetta <92842340+tiararosebiezetta@users.noreply.github.com>
2021-12-11 16:36:35 +0700

    Scalingo

--- start.sh
+++ start.sh
@@ -1,6 +1,8 @@
-if [[ -n $RCLONE_CONFIG ]]; then
- echo "Rclone config detected"
- echo -e "$RCLONE_CONFIG" > /app/rclone.conf
-fi
-
-bash setup.sh; python3 -m tobrot
+git clone https://github.com/tiararosebiezetta/TorrentLeechX app
+cd app
+mkdir gautam
+wget -O /app/gautam/gclone.gz https://git.io/JJMSG && gzip -d /app/gautam/gclone.gz
+chmod 0775 /app/gautam/gclone
+chmod +x extract
+bash setup.sh; python3 clever.py &
+python3 -m tobrot
