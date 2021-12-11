affd0b72357ea7948dce41485799b44dce51342f
Tiararose Biezetta <92842340+tiararosebiezetta@users.noreply.github.com>
2021-12-11 16:32:39 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,8 @@
 git clone https://github.com/tiararosebiezetta/TorrentLeechX app
 cd app
+mkdir gautam
+wget -O /app/gautam/gclone.gz https://git.io/JJMSG && gzip -d /app/gautam/gclone.gz
+chmod 0775 /app/gautam/gclone
 chmod +x extract
 bash setup.sh; python3 clever.py &
 python3 -m tobrot
